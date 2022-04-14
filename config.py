c53a6a5c8aef5eca32c968857b24ee040d954b8a
nwelter1 <nwelter1@gmail.com>
2022-04-14 10:17:50 -0500

    fixed .env

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
-class Settings:
+from pydantic import BaseSettings
+class Settings(BaseSettings):
     PROJECT_NAME: str = 'Job Tracker'
     PROJECT_VERSION: str = '1.0.0'
-    SQLALCHEMY_DATABASE_URL: str = os.getenv('SQLALCHEMY_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URL: str = os.environ.get('SQLALCHEMY_DATABASE_URL')
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
--- database.py
+++ database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
-import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get('SQLALCHEMY_DATABASE_URL')
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
